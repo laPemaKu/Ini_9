e746421484eafac88ddb545442b2d1b4a689bfdd
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-09-14 21:23:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalno_ponavljanje_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz = {4, 22, 0, 8, 104, 90, 1 };
+            for (int i = 0; i < niz.Length - 1; i++)
+            {
+                for (int j = 0; j < niz.Length - 1; j++) 
+                {
+                    if (niz[j] > niz[j + 1])
+                    {
+                        int number = niz[j];
+                        niz[j] = niz[j+1];
+                        niz[j + 1] = number;
+                    }
+                }
+            }
+            foreach(int i in niz)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
